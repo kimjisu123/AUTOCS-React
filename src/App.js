985ff9746d320025fb5b319b68e7d055843c7c48
miji <mjyeo6031@gmail.com>
2023-08-17 18:09:28 +0900

    재고관리페이지생성

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Main from "./pages/Main";
 import Layout from "./pages/layouts/layout";
 import Approval from './pages/approvalPage/Approval'
+import Stock from './pages/stock/Stock'
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
           <Routes>
               <Route path='/' element={<Layout/>}>
                     <Route path='approval' element={<Approval/>}/>
+                    <Route path='stock' element={<Stock/>}/>
               </Route>
           </Routes>
         </BrowserRouter>
