3b73b9b5a9f6e96d16a06f96a1fbbc38ad6d7c88
KimHaedeun <eun06151@naver.com>
2023-08-17 18:12:55 +0900

    로그인 페이지 수정

--- src/pages/layouts/LoginLayout.js
+++ src/pages/layouts/LoginLayout.js
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import Header from "../compoments/Login/Header";
+import Header from "../compoments/LoginHeader/Header";
 
 function LoginLayout() {
     return (
