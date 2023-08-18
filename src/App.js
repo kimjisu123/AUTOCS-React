98d19f3d723a5a6c8bff9ba4c396afbb8fc2ef2e
KimHaedeun <eun06151@naver.com>
2023-08-18 15:47:09 +0900

    로그인 페이지 완성

--- src/App.js
+++ src/App.js
@@ -2,23 +2,27 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./pages/layouts/layout";
 import Approval from './pages/approvalPage/Approval'
 import Login from "./pages/Login/Login";
-import Loginlayout from "./pages/layouts/Loginlayout";
+import FindId from "./pages/Login/findId";
+import FindPwd from "./pages/Login/findPwd";
+import FindOk from "./pages/Login/findOk";
 
 
 function App() {
   return (
       <>
-        <BrowserRouter>
-          <Routes>
-              <Route path='/login' element={<Loginlayout/>}>
-                  <Route path='/login' element={<Login/>}/>
-              </Route>
+          <BrowserRouter>
+              <Routes>
+                  <Route path='/' element={<Layout/>}>
+                      <Route path='approval' element={<Approval/>}/>
+                  </Route>
 
-              <Route path='/' element={<Layout/>}>
-                    <Route path='approval' element={<Approval/>}/>
-              </Route>
-          </Routes>
-        </BrowserRouter>
+                  <Route path='login' element={<Login/>}/>
+                  <Route path='/login/findId' element={<FindId/>}/>
+                  <Route path='/login/findPwd' element={<FindPwd/>}/>
+                  <Route path='/login/findOk' element={<FindOk/>}/>
+
+              </Routes>
+          </BrowserRouter>
       </>
   );
 }
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,5 +1,7 @@
 import './Login.css'
 import img from './loginMain.png'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
@@ -9,22 +11,22 @@ const Login = () => {
                 <img src={img} style={{ width: "145px", height: "200px", marginTop: "-150px", marginBottom: "-35px" }} />
 
                 <h1 style={{ color: "#1C2C10" }}>로그인</h1>
-                <div className="separator"></div>
+                <div className="separator" style={{width: "20%"}}></div>
 
-                <label style={{ marginBottom: "10px", marginTop: "10px", background: "white" }}>아이디</label>
+                <h4 style={{ marginBottom: "10px", marginTop: "10px", background: "white"}}>아이디</h4>
                 <input type="text" id="id" name="id" required />
 
-                <label style={{ marginBottom: "10px", marginTop: "5px", background: "white" }}>비밀번호</label>
+                <h4 style={{ marginBottom: "10px", marginTop: "5px", background: "white" }}>비밀번호</h4>
                 <input type="password" id="pwd" name="pwd" required />
 
                 <button type="login">로그인</button>
 
                 <div style={{ display: "flex", justifyContent: "space-between", marginTop: "15px" }}>
-                    <div style={{ display: "flex", alignItems: "center", marginRight: "-255px", marginTop: "-21px" }}>
+                    <div style={{ display: "flex", alignItems: "center", marginRight: "-255px", marginTop: "-22px" }}>
                         <input type="checkbox" id="accountSave" name="accountSave"/>
                         <h4>계정 저장</h4>
                     </div>
-                    <a href="#" style={{ color: "#1C2C10", marginRight: "170px" }}>아이디/비밀번호 찾기</a>
+                    <Link to="/login/findId" style={{ color: "#1C2C10", marginRight: "170px", marginLeft: "380px" }}>아이디/비밀번호 찾기</Link>
                 </div>
             </main>
         </div>
