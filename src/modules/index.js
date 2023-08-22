c1d08b0b840d25030685c8eed8e273f4756f46ea
miji <mjyeo6031@gmail.com>
2023-08-22 15:49:41 +0900

    재고 리듀서 추가

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,6 +1,7 @@
 // index.js
 import { combineReducers } from 'redux';
 import memberReducer from './MemberModule';
+import stockReducer from './StockModule';
 
 const rootReducer = combineReducers({
     memberReducer,
