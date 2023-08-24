d4550d9c5ddbb48bfd57fa5601f9c361104d0cd6
DESKTOP-TFLQRBN\jihopark <wlghsms987@gmail.com>
2023-08-24 15:52:09 +0900

    jiho

--- src/modules/index.js
+++ src/modules/index.js
@@ -5,8 +5,8 @@ import chartReducer from './ChartModule';
 import stockReducer from './StockModule';
 
 const rootReducer = combineReducers({
-    member: memberReducer, // 이 부분을 수정하여 리듀서를 사용할 수 있도록 해야 합니다.
-    chart: chartReducer
+ // 이 부분을 수정하여 리듀서를 사용할 수 있도록 해야 합니다.
+    chartReducer,
     memberReducer,
     stockReducer
 });
