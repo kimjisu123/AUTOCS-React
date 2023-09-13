1c497ffb0c72ddfbd0620fa7daa9252a13c92623
DESKTOP-TFLQRBN\jihopark <wlghsms987@gmail.com>
2023-09-13 11:59:35 +0900

    jiho

--- src/pages/approvalPage/ApprovalMenubar.js
+++ src/pages/approvalPage/ApprovalMenubar.js
@@ -13,7 +13,7 @@ function ApprovalMenubar() {
 
     return(
         <>
-            <div className={style.menubar} style={{width: "295px", height: "931px",  paddingTop: "30px"}}>
+            <div className={style.menubar} style={{width: "295px", height: "93 paddingTop: "30px"}}>
                     <Link to='/approval' style={{textDecoration:"none"}}><div className={style.newApp}>전자결재</div></Link>
                     <br/><br/>
             <div className={style.documentForm}>
